f3ad443ca242a2cbab2705492a3208173d3abf59
Galenkar13 <joazicardi@hotmail.com>
2018-04-26 22:42:11 +0100

    26/04

--- dll/dll.h
+++ dll/dll.h
@@ -10,9 +10,17 @@
 #define MAX 50 //tamanho do buffer 
 #define SIZE 20 //dimensão da mensagem e do nome 
 
+
+#define nVidas
 #define NumClientes
 #define NumInvaders
-
+#define velocidadeTiro
+#define velocidadeDefensor
+#define velocidadeInvaderBase //A esquiva tem velocidadeInvaderBase + (velocidadeInvaderBase*0.10)
+#define velocidadePowerUP
+#define ProbabilidadeTiroInvader //Esquiva ProbabilidadeTiroInvader - (ProbabilidadeTiroInvader * 0.40)
+#define DuracaoPowerUP //igual para todos 
+#define NumPowerUP
 
 /*
 //Mecanismos de sincronização para o buffer
@@ -46,13 +54,31 @@ typedef enum _TipoMensagemSER {
 } TipoMensagemSER;
 
 typedef enum CicloDeVida {
-	//Isto está no enunciado
+	CRIACAO,
+	ASSOCIACAO,
+	INICIO,
+	DECORRER,
+	FINAL
 } CicloDeVida;
 
 typedef enum TipoPowerUP {
-	//ver lista do enunciado
+	ESCUDO,
+	GELO,
+	BATERIA,
+	MAIS,
+	VIDA,
+	ALCOOL,
+	OUTRO
 } TipoPowerUP;
 
+typedef enum TipoInvader {
+	BASICO,
+	ESQUIVO,
+	EXTRA
+} TipoInvader;
+
+
+ 
 //Quase todos os elementos do jogo são rectangulos, por isso esta estrutura faz sentido
 typedef struct Area {
 	int x, y;
@@ -62,18 +88,26 @@ typedef struct Area {
 typedef struct Pontuacao {
 	int pontos;
 	TCHAR quem[SIZE];
-	//falta aqui a cena da data que não me lembro
+ //falta aqui a cena da data que não me lembro
 } Pontuacao;
 
 //Array de Invaders, Defenders, Disparos? PowerUps? 
 typedef struct Jogo {
+
+	Area Janela;
+
+	//Invader Invaders[NumInvaders];
+
+	//Defender Defenders[NumClientes];
+
+	//Ver como são os power ups e os disparos !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 }Jogo;
 
 typedef struct Invader {
 	Area area;
 	int vidas;
-
+	int velocidade;
 } Invader;
 
 typedef struct Defender {
@@ -81,29 +115,33 @@ typedef struct Defender {
 	int id;
 	Pontuacao pontos;
 	int vidas;
+	int velocidade;
 } Defender;
 
 //As bombas e os tiros têm uma estrutura práticamente igual, portanto ficam na mesma estrutura
 typedef struct Disparos {
 	int x, y; //só ocupam um ponto por isso são não se usa area
-	bool direcao; //As bombas descem e os tiros sobe 
-	//bombas ficam com 0 tiros ficam com 1 !!!!!!!!!!!! Não sei se não é TRUE or FALSE
+	bool direcao; //As bombas sobes e os tiros descem
+	//bombas ficam com 1 tiros ficam com 0 !!!!!!!!!!!! Não sei se não é TRUE or FALSE
 	TCHAR dono[SIZE]; //para incrementar a pontuação dos clientes
+	int velocidade;
 } Disparos;
 
 typedef struct PowerUP {
 	TipoPowerUP tipo;
 	Area area;
 	int duracao; //cada powerup tem efeito durante x tempo
+	int probabilidade;
+	int velocidade;
 } PowerUP;
 
 
 
 //Estrutura para defenir dimensões do "mapa"
 //Se virmos que o mapa nãovai ter mais nada para alem das dimensoes não vale a pena estar aqui
-typedef struct Window { 
+//typedef struct Window { 
 
-} Window;
+//} Window;
 
 //Mensagem que vai do Cliente para o Gateway
 typedef struct MsgCLI {
@@ -115,4 +153,8 @@ typedef struct MsgCLI {
 //Mensagem que vai do Gateway para o cliente
 typedef struct MsgSER {
 	TipoMensagemSER mensagem;
+	int id;
+	TCHAR nome[SIZE];
+
+	
 } MsgSER;
