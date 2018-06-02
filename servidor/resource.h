b1f87ccc3cbfaad047171cbafc676abb0a84cc2b
Galenkar13 <joazicardi@hotmail.com>
2018-06-02 19:39:50 +0100

    Parte grafica servidor começada

---
+++ servidor/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by servidor.rc
+//
+#define IDR_MENU1_Servidor              101
+#define IDD_DIALOG1_configurar          102
+#define IDD_DIALOG1                     104
+#define IDD_DIALOG1_Iniciar             104
+#define ID_NOVOJOGO_CONFIGURAR          40001
+#define ID_NOVOJOGO_INCIAR              40002
+#define ID_NOVOJOGO_SAIR                40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
